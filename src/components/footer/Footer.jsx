5cce9b93f3989e4f27a1dff88d65fcb36442a5dd
Otabek <abdullayevriza80@gmail.com>
2026-04-22 17:32:19 +0500

    qwert

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,48 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer>
-      <hr />
-        <div className=' flex items-center justify-between mt-[60px]'>
-          <ul>
-            <li className='text-[#222222] text-[16px] font-[400] mb-[15px]'>Learn</li>
-             <li className='text-[#222222] text-[16px] font-[400] mt-[15px]'>Help center</li>
-              <li className='text-[#222222] text-[16px] font-[400] mt-[15px]'>Join ZEN team</li>
-              <li className='text-[#222222] text-[16px] font-[400] mt-[15px]'>Privacy Policy</li>
-              <li className='text-[#222222] text-[16px] font-[400] mt-[15px]'>Cookies</li>
-              <li className='text-[#222222] text-[16px] font-[400] mt-[15px]'>Terms of use</li>
-          </ul>
-
-          <ul>
-            <li className='text-[#222222] text-[16px] font-[400] mb-[15px]'>Discover</li>
-             <li className='text-[#222222] text-[16px] font-[400] mt-[15px]'>For developers</li>
-              <li className='text-[#222222] text-[16px] font-[400] mt-[15px]'>Apple Pay</li>
-              <li className='text-[#222222] text-[16px] font-[400] mt-[15px]'>Google Pay</li>
-          </ul>
-
-          <ul>
-            <li className='text-[#222222] text-[16px] font-[400] mb-[15px]'>Offer</li>
-             <li className='text-[#222222] text-[16px] font-[400] mt-[15px]'>Business</li>
-              <li className='text-[#222222] text-[16px] font-[400] mt-[15px]'>Personal</li>
-          </ul>
-
-          <ul>
-            <li className='text-[#222222] text-[16px] font-[400] mb-[15px]'>Language</li>
-             <li className='text-[#222222] text-[16px] font-[400] mt-[15px]'>English</li>
-              <li className='text-[#222222] text-[16px] font-[400] mt-[15px]'>Polish</li>
-          </ul>
-
-          <ul>
-            <li className='text-[#222222] text-[16px] font-[400] mb-[15px]'>Follow us</li>
-          </ul>
-
-          <ul>
-            <li className='text-[#222222] text-[16px] font-[400] mb-[15px]'>Contact</li>
-             <li className='text-[#222222] text-[16px] font-[400] mt-[15px]'>hello@zen.com</li>
-              
-          </ul>
-        </div>
-    </footer>
+    
   )
 }
 

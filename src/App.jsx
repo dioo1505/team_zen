00b1e695837bdbc7699bdca932895bfe45f66ca4
dioo1505 <diyorakenjaboyeva1702@gmail.com>
2026-04-22 17:04:17 +0500

    added header section

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
+import Boost from './components/boost/Boost'
+import Cashback from './components/cashback/Cashback'
+import Currencies from './components/currencies/Currencies'
+import Easy from './components/easy/Easy'
+import Fees from './components/fees/Fees'
+import Footer from './components/footer/Footer'
+import Header from './components/header/Header'
+import Hero from './components/hero/Hero'
+import MasterCard from './components/mastercard/MasterCard'
+import Zen from './components/zen/Zen'
 
 const App = () => {
   return (
     <div>
-
+      <Header/>
+      <Hero/>
+      <Boost/>
+      <Zen/>
+      <Cashback/>
+      <MasterCard/>
+      <Fees/>
+      <Currencies/>
+      <Easy/>
+      <Footer/>
     </div>
   )
 }
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,45 @@
-import React from 'react'
+import logo from '../../assets/logo.png';
 
 const Header = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <header className="w-full h-[55px] flex items-center justify-between px-[75px] py-[10px] border-b border-b-gray-500">
+      <div className="header__left flex items-center gap-[30px]">
+        <img src={logo} alt="" />
+        <ul className="flex items-center gap-[22px]">
+          <li>
+            <a className="text-[#222222] text-[16px] nunito" href="/">
+              Personal
+            </a>
+          </li>
+          <li>
+            <a className="text-[#222222] text-[16px] nunito" href="/">
+              Bussiness
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className="header__right flex items-center gap-[40px]">
+        <ul className="flex items-center gap-[30px]">
+          <li>
+            <a className="text-[#222222] text-[16px] nunito" href="/">
+              Zenefits
+            </a>
+          </li>
+          <li>
+            <a className="text-[#222222] text-[16px] nunito" href="/">
+              Features
+            </a>
+          </li>
+          <li>
+            <a className="text-[#222222] text-[16px] nunito" href="/">
+              Pricing
+            </a>
+          </li>
+        </ul>
+        <button className="text-[18px] text-[#222222] nunito border rounded-[48px] py-[8px] px-[32px]">Get app</button>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
